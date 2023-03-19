f04539ccf02c9ea571b5abc50090525f99858edf
Aime Fabrice <aimefabrigas@gmail.com>
2023-03-19 14:09:39 +0200

    remembered somethings

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - runtime entry point
+ *
+ * The program does
+ * Return: int 1, if successful
  */
 int main(void)
 {
-fwrite(STDOUT_FILENO, "and that piece of art is useful\"
-		- Dora Korpar 2015-10-19\n", 59);
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+59, 1, stderr);
 return (1);
 }
-
