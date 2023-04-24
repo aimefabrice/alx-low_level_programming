1ba0e7cf716b1756217213e402b6fb937bd25a62
Aime Fabrice <aimefabrigas@gmail.com>
2023-04-24 11:07:31 +0200

    Variadic Function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,22 +9,22 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list ap;
-    if (separator == NULL)
-    separator = "";
-	va_start(ap, n);
+unsigned int i;
+va_list ap;
+if (separator == NULL)
+separator = "";
+va_start(ap, n);
 
-	for (i = 1; i <= n; i++)
+for (i = 1; i <= n; i++)
+{
+	printf("%i", va_arg(ap, int));
+	if (i < n && separator)
+		printf("%s", separator);
+	else
 	{
-		printf("%i", va_arg(ap, int));
-		if (i < n && separator)
-			printf("%s", separator);
-		else
-		{
-			;
-		}
+		;
 	}
-	printf("\n");
-	va_end(ap);
+}
+printf("\n");
+va_end(ap);
 }
